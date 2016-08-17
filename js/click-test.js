1a32fe412dc7ca1446dcb1f8f53c9859933eb833
Jason Cemra <cemrajc@users.noreply.github.com>
2016-08-17 18:01:22 +1000

    Added: Responsive design for click tester

--- js/click-test.js
+++ js/click-test.js
@@ -8,6 +8,12 @@ function logClickEvent(msg, button) {
     click_log.scrollTop = 100000; // A hacky way of sticking scroll to the bottom
 }
 
+function logTouchEvent(msg) {
+    var full_message_string = new Date().toTimeString().match(/^[0-9:]{2,10}/)[0] + ":  <b>" + msg + "</b>, why are you testing your finger?";
+    click_log.innerHTML = click_log.innerHTML + full_message_string + "<br>";
+    click_log.scrollTop = 100000; // A hacky way of sticking scroll to the bottom
+}
+
 function buttonDecode(number) {
     if (typeof number !== "number") {
         return false;
@@ -37,6 +43,16 @@ clicker.addEventListener('mousedown', function(e){
 clicker.addEventListener('mouseup', function(e){
     e.preventDefault();
     logClickEvent("mouse up", buttonDecode(e.buttons));
+});
+
+clicker.addEventListener('touchstart', function(e){
+    e.preventDefault();
+    logTouchEvent("touch start");
+});
+
+clicker.addEventListener('touchend', function(e){
+    e.preventDefault();
+    logTouchEvent("touch end");
 });
 
 clicker.addEventListener('contextmenu', function(e){
